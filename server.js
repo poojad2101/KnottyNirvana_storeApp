731394196f68e3611bb554090789ef3a55538c01
poojad2101 <davepooja2101@gmail.com>
2022-06-11 13:26:22 +0530

    required dependencies

---
+++ server.js
@@ -0,0 +1,6 @@
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const mongoose = require("mongoose")
+const path = require("path")
