257907be01ea1583c6b4f3f86294ff0c4207ba38
poojad2101 <davepooja2101@gmail.com>
2022-06-12 11:15:52 +0530

    created connection to database through mongoose

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
-const methodOverride = require("method-override")
-const mongoose = require("mongoose")
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 const path = require("path")
+
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+//Establish Connection
+mongoose.connect(DATABASE_URL, CONFIG);
+
+mongoose.connection
+.on("open", () => console.log("Connected to Mongoose"))
+.on("close", () => console.log("Disconnected from Mongoose"))
+.on("error", (error) => console.log(error));
