f7ce105957f0ecc160846380532b03c76ae57632
poojad2101 <davepooja2101@gmail.com>
2022-06-12 13:27:28 +0530

    created product model

--- server.js
+++ server.js
@@ -19,3 +19,56 @@ mongoose.connection
 .on("open", () => console.log("Connected to Mongoose"))
 .on("close", () => console.log("Disconnected from Mongoose"))
 .on("error", (error) => console.log(error));
+
+
+// const Schema = mongoose.Schema
+// const model = mongoose.model
+
+const {Schema, model } = mongoose;
+
+//Make productsSchema
+const productsSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: String,
+    img: String,
+    price: {
+        type: Number,
+        min: 0
+    },
+    qty: {
+        type: Number,
+        min: 0
+    }
+})
+//Make product model
+const Product = model('Product', productsSchema)
+
+
+/////////////////////////////////////////////////
+// Create our Express Application Object
+/////////////////////////////////////////////////
+const app = require("liquid-express-views")(express(), {root: [path.resolve(__dirname, 'views/')]})
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); //logging
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
+app.use(express.static("public")); // serve files from public statically
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+app.get("/", (req, res) => {
+  res.send("your server is running... better catch it.");
+});
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
