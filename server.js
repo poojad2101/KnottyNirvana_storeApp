27c5ad6d85bca92334362588df0f77f62545a3e0
poojad2101 <davepooja2101@gmail.com>
2022-06-14 03:50:07 +0530

    delete route created, delete button working

--- server.js
+++ server.js
@@ -198,7 +198,23 @@ app.put("/products/:id", (req, res) => {
       });
   });
   
-
+  app.delete("/products/:id", (req, res) => {
+    // get the id from params
+    const id = req.params.id;
+    // delete the fruit
+    Product.findByIdAndRemove(id)
+      .then((product) => {
+        // redirect to main page after deleting
+        res.redirect("/products");
+      })
+      // send error as json
+      .catch((error) => {
+        console.log(error);
+        res.json({ error });
+      });
+  });
+  
+  
 //////////////////////////////////////////////
 // Server Listener
 //////////////////////////////////////////////
