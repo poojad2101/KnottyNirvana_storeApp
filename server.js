114ef7fb1a6dec086e207bb1947c84b760d73729
poojad2101 <davepooja2101@gmail.com>
2022-06-15 00:22:07 +0530

    created liquid files for user

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const mongoose = require("./models/connection");
 const Product = require("./models/products")
 const ProductRouter = require("./controllers/product");
 const ReviewRouter = require("./controllers/review");
+const UserRouter = require("./controllers/user");
+
 
 
 
@@ -34,6 +36,7 @@ app.use(express.static("public")); // serve files from public statically
 ////////////////////////////////////////////
 app.use("/products", ProductRouter);
 app.use("/reviews", ReviewRouter);
+app.use("/user", UserRouter);
 
 app.get("/", (req, res) => {
     res.send("your server is running... better catch it.");
