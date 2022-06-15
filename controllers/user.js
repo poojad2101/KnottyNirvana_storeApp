0680f989b91f61061a49529091a0e799953b5f4d
poojad2101 <davepooja2101@gmail.com>
2022-06-15 13:47:30 +0530

    made sign and login post response

--- controllers/user.js
+++ controllers/user.js
@@ -19,18 +19,57 @@ router.get("/signup", (req, res) => {
   res.render("user/signup.liquid");
 });
 
-router.post("/signup", (req, res) => {
-  res.send("signup");
+router.post("/signup", async (req, res) => {
+  // encrypt password
+  req.body.password = await bcrypt.hash(
+    req.body.password,
+    await bcrypt.genSalt(10)
+  );
+  // create new user
+  User.create(req.body)
+    .then((user) => {
+      // redirect to login page
+      res.redirect("/user/login");
+    })
+    .catch((error) => {
+      // send error as json
+      console.log(error);
+      res.json({ error });
+    });
 });
+  
 
 // The login Routes (Get => form, post => submit form)
-router.get("/login", (req, res) => {
-  res.render("user/login.liquid");
+router.get("/login", async (req, res) => {
+  // get the data from the request body
+  const { username, password } = req.body;
+  // search for the user
+  User.findOne({ username })
+    .then(async (user) => {
+      // check if user exists
+      if (user) {
+        // compare password
+        const result = await bcrypt.compare(password, user.password);
+        if (result) {
+          // redirect to fruits page if successful
+          res.redirect("/fruits");
+        } else {
+          // error if password doesn't match
+          res.json({ error: "password doesn't match" });
+        }
+      } else {
+        // send error if user doesn't exist
+        res.json({ error: "user doesn't exist" });
+      }
+    })
+    .catch((error) => {
+      // send error as json
+      console.log(error);
+      res.json({ error });
+    });
 });
 
-router.post("/login", (req, res) => {
-  res.send("login");
-});
+
 
 //////////////////////////////////////////
 // Export the Router
